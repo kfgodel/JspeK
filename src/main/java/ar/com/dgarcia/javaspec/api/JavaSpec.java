6b7552b994b72bb6215bd8c3d93ae133e686d338
dario.garcia <kfgodel@gmail.com>
2015-12-31 19:41:50 -0300

    Segregated interpreter from javaspec interaction class

--- src/main/java/ar/com/dgarcia/javaspec/api/JavaSpec.java
+++ src/main/java/ar/com/dgarcia/javaspec/api/JavaSpec.java
@@ -1,121 +1,72 @@
 package ar.com.dgarcia.javaspec.api;
 
-import ar.com.dgarcia.javaspec.impl.context.typed.TypedContextFactory;
-import ar.com.dgarcia.javaspec.impl.exceptions.SpecException;
-import ar.com.dgarcia.javaspec.impl.model.SpecGroup;
 import ar.com.dgarcia.javaspec.impl.model.SpecTree;
-import ar.com.dgarcia.javaspec.impl.model.impl.GroupSpecDefinition;
-import ar.com.dgarcia.javaspec.impl.model.impl.SpecTreeDefinition;
-import ar.com.dgarcia.javaspec.impl.model.impl.TestSpecDefinition;
-import ar.com.dgarcia.javaspec.impl.parser.SpecStack;
-
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.Optional;
+import ar.com.dgarcia.javaspec.impl.parser.SpecInterpreter;
 
 /**
  * This class is the extension point to add testing expressiveness with Java Specs.<br>
- *     The method idiom is copied from: http://jasmine.github.io/2.0/introduction.html.<br>
+ * The method idiom is copied from: http://jasmine.github.io/2.0/introduction.html.<br>
  * Created by kfgodel on 12/07/14.
  */
 public abstract class JavaSpec<T extends TestContext> implements JavaSpecApi<T> {
 
-    private SpecStack stack;
-    private Variable<TestContext> sharedContext;
-    private T typedContext;
+  private SpecInterpreter<T> interpreter;
 
-    /**
-     * Starting method to define the specs.<br>
-     *     This method must be extended by subclasses and define any spec as calls to describe() and it()
-     */
-    public abstract void define();
+  /**
+   * Starting method to define the specs.<br>
+   * This method must be extended by subclasses and define any spec as calls to describe() and it()
+   */
+  public abstract void define();
 
-    @Override
-    public void beforeEach(Runnable aCodeBlock) {
-        stack.getCurrentGroup().addBeforeBlock(aCodeBlock);
-    }
+  /**
+   * Creates a spec definition tree with the specification defined by the user in the subclass
+   */
+  public SpecTree defineTree() {
+    // Needs to be instance variable to be accesed in the define method
+    this.interpreter = SpecInterpreter.create(this.getClass());
 
-    @Override
-    public void afterEach(Runnable aCodeBlock) {
-        stack.getCurrentGroup().addAfterBlock(aCodeBlock);
-    }
+    this.define();
 
-    @Override
-    public void it(String testName, Runnable aTestCode){
-        addTestDefinition(testName, Optional.of(aTestCode));
-    }
+    return interpreter.getSpecTree();
+  }
 
-    @Override
-    public void it(String testName){
-        addTestDefinition(testName, Optional.empty());
-    }
+  @Override
+  public T context() {
+    return interpreter.context();
+  }
 
-    @Override
-    public void xit(String testName, Runnable aTestCode){
-        TestSpecDefinition createdTestDefinition = addTestDefinition(testName, Optional.of(aTestCode));
-        createdTestDefinition.markAsPending();
-    }
+  @Override
+  public void beforeEach(Runnable aCodeBlock) {
+    interpreter.beforeEach(aCodeBlock);
+  }
 
-    private TestSpecDefinition addTestDefinition(String testName, Optional<Runnable> testBody) {
-        TestSpecDefinition createdSpec = TestSpecDefinition.create(testName, testBody, sharedContext);
-        stack.getCurrentGroup().addSubElement(createdSpec);
-        return createdSpec;
-    }
+  @Override
+  public void afterEach(Runnable aCodeBlock) {
+    interpreter.afterEach(aCodeBlock);
+  }
 
+  @Override
+  public void it(String testName, Runnable aTestCode) {
+    interpreter.it(testName, aTestCode);
+  }
 
-    @Override
-    public void describe(String aGroupName, Runnable aGroupDefinition){
-        nestGroupDefinition(aGroupName, aGroupDefinition);
-    }
+  @Override
+  public void it(String testName) {
+    interpreter.it(testName);
+  }
 
-    @Override
-    public void xdescribe(String aGroupName, Runnable aGroupDefinition){
-        GroupSpecDefinition createdGroup = nestGroupDefinition(aGroupName, aGroupDefinition);
-        createdGroup.markAsDisabled();
-    }
+  @Override
+  public void xit(String testName, Runnable aTestCode) {
+    interpreter.xit(testName, aTestCode);
+  }
 
-    private GroupSpecDefinition nestGroupDefinition(String aGroupName, Runnable definitionBlock) {
-        GroupSpecDefinition createdGroup = GroupSpecDefinition.create(aGroupName);
-        stack.runNesting(createdGroup, definitionBlock::run);
-        return createdGroup;
-    }
+  @Override
+  public void describe(String aGroupName, Runnable aGroupDefinition) {
+    interpreter.describe(aGroupName, aGroupDefinition);
+  }
 
-
-    /**
-     * Creates a spec definition tree with the specification defined by the user in the subclass
-     */
-    public SpecTree defineTree() {
-        SpecTree specTree = SpecTreeDefinition.create(this.getClass());
-
-        this.sharedContext = Variable.create();
-        SpecGroup rootGroup = specTree.getRootGroup();
-        this.stack = SpecStack.create(rootGroup, sharedContext);
-
-        Class<T> expectedContextType = this.getContextTypeFromSubclassDeclaration();
-        this.typedContext = TypedContextFactory.createInstanceOf(expectedContextType, sharedContext);
-
-        this.define();
-        return specTree;
-    }
-
-    @Override
-    public T context() {
-        return typedContext;
-    }
-
-    public Class<T> getContextTypeFromSubclassDeclaration() {
-        Class<? extends JavaSpec> subClass = getClass();
-        if(!(subClass.getSuperclass().equals(JavaSpec.class))){
-            throw new SpecException("A java spec class["+ subClass+ "] must inherit directly from " +JavaSpec.class);
-        }
-        Type generifiedJavaSpec = subClass.getGenericSuperclass();
-        if(!(generifiedJavaSpec instanceof ParameterizedType)){
-            throw new SpecException("JavaSpec superclass must be generified with a type of TestContext. For example JavaSpec<TestContext>");
-        }
-        Type contextType = ((ParameterizedType) generifiedJavaSpec).getActualTypeArguments()[0];
-        if(!(contextType instanceof Class)){
-            throw new SpecException("JavaSpec parameter is not a class?");
-        }
-        return (Class<T>) contextType;
-    }
+  @Override
+  public void xdescribe(String aGroupName, Runnable aGroupDefinition) {
+    interpreter.xdescribe(aGroupName, aGroupDefinition);
+  }
 }
---
+++ src/main/java/ar/com/dgarcia/javaspec/impl/parser/SpecInterpreter.java
@@ -0,0 +1,125 @@
+package ar.com.dgarcia.javaspec.impl.parser;
+
+import ar.com.dgarcia.javaspec.api.JavaSpec;
+import ar.com.dgarcia.javaspec.api.JavaSpecApi;
+import ar.com.dgarcia.javaspec.api.TestContext;
+import ar.com.dgarcia.javaspec.api.Variable;
+import ar.com.dgarcia.javaspec.impl.context.typed.TypedContextFactory;
+import ar.com.dgarcia.javaspec.impl.exceptions.SpecException;
+import ar.com.dgarcia.javaspec.impl.model.SpecGroup;
+import ar.com.dgarcia.javaspec.impl.model.SpecTree;
+import ar.com.dgarcia.javaspec.impl.model.impl.GroupSpecDefinition;
+import ar.com.dgarcia.javaspec.impl.model.impl.SpecTreeDefinition;
+import ar.com.dgarcia.javaspec.impl.model.impl.TestSpecDefinition;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Optional;
+
+/**
+ * This type represents the interpreter that builds the spec tree when api methods are called,
+ * interpreting the users input as test specificiations
+ *
+ * Created by tenpines on 31/12/15.
+ */
+public class SpecInterpreter<T extends TestContext> implements JavaSpecApi<T> {
+
+  private SpecTreeDefinition specTree;
+  private Variable<TestContext> sharedContext;
+  private SpecStack stack;
+  private T typedContext;
+  private Class<?> specClass;
+
+  public static<T extends TestContext> SpecInterpreter<T> create(Class<?> specClass){
+    SpecInterpreter<T> interpreter = new SpecInterpreter<>();
+    interpreter.specClass = specClass;
+    interpreter.initialize();
+    return interpreter;
+  }
+
+  private void initialize() {
+    this.specTree =  SpecTreeDefinition.create(specClass);
+
+    this.sharedContext = Variable.create();
+    SpecGroup rootGroup = specTree.getRootGroup();
+    this.stack = SpecStack.create(rootGroup, sharedContext);
+
+    Class<T> expectedContextType = this.getContextTypeFromSubclassDeclaration();
+    this.typedContext = TypedContextFactory.createInstanceOf(expectedContextType, sharedContext);
+  }
+
+  @Override
+  public void beforeEach(Runnable aCodeBlock) {
+    stack.getCurrentGroup().addBeforeBlock(aCodeBlock);
+  }
+
+  @Override
+  public void afterEach(Runnable aCodeBlock) {
+    stack.getCurrentGroup().addAfterBlock(aCodeBlock);
+  }
+
+  @Override
+  public void it(String testName, Runnable aTestCode){
+    addTestDefinition(testName, Optional.of(aTestCode));
+  }
+
+  @Override
+  public void it(String testName){
+    addTestDefinition(testName, Optional.empty());
+  }
+
+  @Override
+  public void xit(String testName, Runnable aTestCode){
+    TestSpecDefinition createdTestDefinition = addTestDefinition(testName, Optional.of(aTestCode));
+    createdTestDefinition.markAsPending();
+  }
+
+  private TestSpecDefinition addTestDefinition(String testName, Optional<Runnable> testBody) {
+    TestSpecDefinition createdSpec = TestSpecDefinition.create(testName, testBody, sharedContext);
+    stack.getCurrentGroup().addSubElement(createdSpec);
+    return createdSpec;
+  }
+
+
+  @Override
+  public void describe(String aGroupName, Runnable aGroupDefinition){
+    nestGroupDefinition(aGroupName, aGroupDefinition);
+  }
+
+  @Override
+  public void xdescribe(String aGroupName, Runnable aGroupDefinition){
+    GroupSpecDefinition createdGroup = nestGroupDefinition(aGroupName, aGroupDefinition);
+    createdGroup.markAsDisabled();
+  }
+
+  private GroupSpecDefinition nestGroupDefinition(String aGroupName, Runnable definitionBlock) {
+    GroupSpecDefinition createdGroup = GroupSpecDefinition.create(aGroupName);
+    stack.runNesting(createdGroup, definitionBlock::run);
+    return createdGroup;
+  }
+
+  @Override
+  public T context() {
+    return typedContext;
+  }
+
+  public SpecTree getSpecTree() {
+    return specTree;
+  }
+
+  public Class<T> getContextTypeFromSubclassDeclaration() {
+    if(!(specClass.getSuperclass().equals(JavaSpec.class))){
+      throw new SpecException("A java spec class["+ specClass + "] must inherit directly from " +JavaSpec.class);
+    }
+    Type generifiedJavaSpec = specClass.getGenericSuperclass();
+    if(!(generifiedJavaSpec instanceof ParameterizedType)){
+      throw new SpecException("JavaSpec superclass must be generified with a type of TestContext. For example JavaSpec<TestContext>");
+    }
+    Type contextType = ((ParameterizedType) generifiedJavaSpec).getActualTypeArguments()[0];
+    if(!(contextType instanceof Class)){
+      throw new SpecException("JavaSpec parameter is not a class?");
+    }
+    return (Class<T>) contextType;
+  }
+
+}
