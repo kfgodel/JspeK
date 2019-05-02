fb30ceb3add67135703ecee38459a09cc79a3150
Nico <nmrainhart@gmail.com>
2019-05-01 22:27:57 -0300

    Rename Let#set to Let#let
    To be used in Java code

--- src/main/java/ar/com/dgarcia/javaspec/api/variable/Let.java
+++ src/main/java/ar/com/dgarcia/javaspec/api/variable/Let.java
@@ -26,7 +26,7 @@ public class Let<T> {
    *
    * @param definition A value supplier that can be used to lazily define the initial value of the variable
    */
-  public <U extends T> Let<U> set(Supplier<T> definition) {
+  public <U extends T> Let<U> let(Supplier<T> definition) {
     context().let(variableName(), definition);
     return (Let<U>) this;
   }
