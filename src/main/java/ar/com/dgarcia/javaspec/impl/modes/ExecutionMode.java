95969407d100e3bc85ca2a02997f35a178298a97
Nico <nmrainhart@gmail.com>
2019-05-27 21:31:41 -0300

    Add javadoc for execution modes

--- src/main/java/ar/com/dgarcia/javaspec/impl/modes/ExecutionMode.java
+++ src/main/java/ar/com/dgarcia/javaspec/impl/modes/ExecutionMode.java
@@ -3,5 +3,8 @@ package ar.com.dgarcia.javaspec.impl.modes;
 import ar.com.dgarcia.javaspec.api.JavaSpecApi;
 import ar.com.dgarcia.javaspec.api.contexts.TestContext;
 
+/**
+ * This type defines the contract that spec implementations can use during a specific stage of tests' execution
+ */
 public interface ExecutionMode<T extends TestContext> extends JavaSpecApi<T> {
 }
