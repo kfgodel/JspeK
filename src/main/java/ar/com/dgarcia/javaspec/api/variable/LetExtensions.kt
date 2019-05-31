ab0e30853230a7ad4317103ebd1b3e21e93ddc44
Nico <nmrainhart@gmail.com>
2019-05-31 16:06:59 -0300

    Remove no longer used Let extension for Kotlin

--- src/main/java/ar/com/dgarcia/javaspec/api/variable/LetExtensions.kt
+++
@@ -1,6 +0,0 @@
-package ar.com.dgarcia.javaspec.api.variable
-
-fun <T> Let<T>.set(definition: () -> T): Let<T> {
-  context().let(variableName(), definition)
-  return Let.create(variableName()) { context() }
-}
